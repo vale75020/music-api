1f921534f55ff54aea1af796c3e565a1d274ef13
Florent Heyraud <florentheyraud.pro@gmail.com>
2018-07-16 12:26:27 +0200

    fix route

--- src/index.js
+++ src/index.js
@@ -22,7 +22,21 @@ app.get("/", (req, res) => {
     res.send("first route!")
 })
 
+app.use((req, res, next) => {
+    const error = new Error("Not found")
+    error.message = "route invalide"
+    error.status = 404
+    next(error)
+})
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.json({
+        error:{
+            msg: error.message
+        }
+    })
+})
 app.listen(port, () => {
   console.log("Youpi le port fonctionne!")
 })
